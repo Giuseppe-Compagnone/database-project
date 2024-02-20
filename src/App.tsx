f5084bb3e00836c62817aa4bf112f1a17fde7afe
Giuseppe Compagnone <94106986+Giuseppe-Compagnone@users.noreply.github.com>
2024-02-20 21:20:11 +0100

    boilerplate

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./../styles/main.scss";
+
+const App = () => {
+  return <>App</>;
+};
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.tsx",
+  output: {
+    path: path.resolve(__dirname, "public", "build"),
+    filename: "bundle.js",
+  },
+  resolve: { extensions: [".js", ".jsx", ".json", ".ts", ".tsx"] },
+  module: {
+    rules: [
+      { test: /\.(ts|tsx)$/, loader: "ts-loader" },
+      { enforce: "pre", test: /\.js$/, loader: "source-map-loader" },
+      { test: /\.css$/, loader: "css-loader" },
+      {
+        test: /\.s[ac]ss$/i,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "public", "index.html"),
+    }),
+  ],
+};

9fe37b29c0ebb7c176a381e79ea978624da878a9
Giuseppe Compagnone <94106986+Giuseppe-Compagnone@users.noreply.github.com>
2024-02-25 16:49:38 +0100

    http server

--- backend/netlify/functions/api.ts
+++ backend/netlify/functions/api.ts
@@ -1,5 +1,6 @@
 import express, { Router } from "express";
 import serverless from "serverless-http";
+import http from "http";
 
 const api = express();
 
@@ -7,5 +8,14 @@ const router = Router();
 router.get("/hello", (req, res) => res.send("Hello World!"));
 
 api.use("/api/", router);
+
+http
+  .createServer((req, res) => {
+    if (req.url === "/") {
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.end("Welcome to the homepage!");
+    }
+  })
+  .listen(8081, "localhost");
 
 export const handler = serverless(api);

c3c7cd47724be78bf7bc07b77e39e0ac07503c7a
Giuseppe Compagnone <94106986+Giuseppe-Compagnone@users.noreply.github.com>
2024-02-26 22:15:55 +0100

    teacher pages

--- frontend/src/pages/CoursePage/CoursePage.tsx
+++ frontend/src/pages/CoursePage/CoursePage.tsx
@@ -1,14 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import { Material } from "./components";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faEnvelope,
+  faHeading,
+  faLink,
+} from "@fortawesome/free-solid-svg-icons";
+import { faFileLines } from "@fortawesome/free-regular-svg-icons";
+import { Button } from "../../components";
 
 const CoursePage = () => {
+  //States
+  const [evaluation, setEvaluation] = useState<string>("");
+
   return (
     <div className="course-page">
       <div className="header">
         <div className="teacher">prof. teacher</div>
         <h1 className="title">Course Name</h1>
         <p className="desc">description</p>
-        <div className="enrolleds">enrolleds: 0</div>
+        <div className="enrolleds">Enrolleds: 0</div>
       </div>
       <h1 className="title">Materials</h1>
       <div className="materials">
@@ -31,6 +42,86 @@ const CoursePage = () => {
           postDate={"2024/02/26"}
         />
       </div>
+      <h1 className="title">Upload Material</h1>
+      <form
+        className="form"
+        onSubmit={(e) => {
+          e.preventDefault();
+          console.log("Submit");
+        }}
+      >
+        <div className="field-wrapper">
+          <div className="icon">
+            <FontAwesomeIcon icon={faHeading} />
+          </div>
+          <label htmlFor="name">Name</label>
+          <input type="text" placeholder="Material name..." name="name" />
+        </div>
+        <div className="field-wrapper">
+          <div className="icon textarea">
+            <FontAwesomeIcon icon={faFileLines} />
+          </div>
+          <label htmlFor="desc">Description</label>
+          <textarea
+            name="desc"
+            placeholder="Material description..."
+            required={false}
+          />
+        </div>
+        <div className="field-wrapper">
+          <div className="icon">
+            <FontAwesomeIcon icon={faLink} />
+          </div>
+          <label htmlFor="link">Link</label>
+          <input type="text" placeholder="Material link..." name="link" />
+        </div>
+        <Button text={"Upload"} onClick={() => {}} />
+      </form>
+      <h1 className="title" style={{ margin: "2rem 0" }}>
+        Upload Student Performance
+      </h1>
+      <form
+        className="form"
+        onSubmit={(e) => {
+          e.preventDefault();
+          console.log("Submit");
+        }}
+      >
+        <div className="field-wrapper">
+          <div className="icon">
+            <FontAwesomeIcon icon={faEnvelope} />
+          </div>
+          <label htmlFor="mail">Email</label>
+          <input type="email" placeholder="Student email..." name="mail" />
+        </div>
+
+        <div className="field-wrapper">
+          <div className="icon">
+            <FontAwesomeIcon icon={faLink} />
+          </div>
+          <label htmlFor="eval">Evaluation</label>
+          <input
+            type="text"
+            placeholder="Student Evaluation"
+            name="eval"
+            value={evaluation}
+            onChange={(e) => {
+              if (e.target.value === "") {
+                setEvaluation("");
+                return;
+              }
+              if (!/^-?\d*$/.test(e.target.value)) return;
+              const evaluation = parseInt(e.target.value);
+              if (evaluation > 30) {
+                setEvaluation("30");
+              } else {
+                setEvaluation(evaluation.toString());
+              }
+            }}
+          />
+        </div>
+        <Button text={"Upload"} onClick={() => {}} />
+      </form>
     </div>
   );
 };
--- frontend/src/pages/CoursesPage/CoursesPage.tsx
+++ frontend/src/pages/CoursesPage/CoursesPage.tsx
@@ -1,8 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import { Course } from "./components";
 import { Link, useNavigate } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faCalendar,
+  faHeading,
+  faPlus,
+} from "@fortawesome/free-solid-svg-icons";
+import { Button } from "../../components";
+import { faFileLines } from "@fortawesome/free-regular-svg-icons";
 
 const CoursesPage = () => {
+  //Methods
+  const getTomorrowDate = () => {
+    const today = new Date();
+    const year = today.getFullYear();
+    const month = String(today.getMonth() + 1).padStart(2, "0");
+    const day = String(today.getDate() + 1).padStart(2, "0");
+    return `${year}-${month}-${day}`;
+  };
+
+  //States
+  const [newCourse, setNewCourse] = useState<string>("");
+  const [startDate, setStartDate] = useState(getTomorrowDate());
+  const [endDate, setDate] = useState(getTomorrowDate());
+
+  //Hooks
   const navigate = useNavigate();
 
   return (
@@ -23,6 +46,71 @@ const CoursesPage = () => {
             navigate("/course/matematica");
           }}
         />
+        <div className="add">
+          <input
+            type="text"
+            placeholder="Enter course name..."
+            value={newCourse}
+            onChange={(e) => {
+              setNewCourse(e.target.value);
+            }}
+          />
+          <Button text={"Add"} onClick={() => {}} />
+        </div>
+      </div>
+      <h1 className="title">Create a Course</h1>
+      <div className="create">
+        <form
+          className="form"
+          onSubmit={(e) => {
+            e.preventDefault();
+            console.log("Submit");
+          }}
+        >
+          <div className="field-wrapper">
+            <div className="icon">
+              <FontAwesomeIcon icon={faHeading} />
+            </div>
+            <label htmlFor="name">Name (unique)</label>
+            <input type="text" placeholder="Course name..." name="name" />
+          </div>
+          <div className="field-wrapper">
+            <div className="icon textarea">
+              <FontAwesomeIcon icon={faFileLines} />
+            </div>
+            <label htmlFor="desc">Description</label>
+            <textarea
+              name="desc"
+              placeholder="Course description..."
+              required={false}
+            />
+          </div>
+          <div className="field-wrapper">
+            <label htmlFor="start">Start date</label>
+            <input
+              type="date"
+              name="start"
+              value={startDate}
+              onChange={(e) => {
+                setStartDate(e.target.value);
+              }}
+              min={getTomorrowDate()}
+            />
+          </div>
+          <div className="field-wrapper">
+            <label htmlFor="end">End date</label>
+            <input
+              type="date"
+              name="end"
+              value={startDate}
+              onChange={(e) => {
+                setStartDate(e.target.value);
+              }}
+              min={getTomorrowDate()}
+            />
+          </div>
+          <Button text={"Create"} onClick={() => {}} />
+        </form>
       </div>
     </div>
   );

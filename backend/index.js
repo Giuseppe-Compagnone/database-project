b2402b42d25dae224d51964d193579d8d4f67929
Giuseppe Compagnone <94106986+Giuseppe-Compagnone@users.noreply.github.com>
2024-02-26 23:25:47 +0100

    database connection

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,16 @@
-const express = require("express");
+import express from "express";
 
 const app = express();
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Something broke!");
+});
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.listen(3000);
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
